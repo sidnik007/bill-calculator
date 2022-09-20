49f8e11e5385846b42d66f2a72dccee640738939
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2022-09-20 11:08:16 +0530

    Use enum.

--- src/main/java/com/nelkinda/BillCalculator.java
+++ src/main/java/com/nelkinda/BillCalculator.java
@@ -1,21 +1,29 @@
 package com.nelkinda;
 
+enum PlanType {
+    PLATINUM("Platinum", 999, 199, 1500, 1.30),
+    GOLD("Gold", 799, 299, 1000, 2.45),
+    SILVER("Silver", 599, 399, 500, 3.60);
+
+    final String name;
+    final int baseRate;
+    final int ratePerAdditionalConnection;
+    final int baseMinutes;
+    final double ratePerExcessMinute;
+
+    PlanType(final String name, int baseRate, final int ratePerAdditionalConnection, final int baseMinutes, final double ratePerExcessMinute) {
+        this.name = name;
+        this.baseRate = baseRate;
+        this.ratePerAdditionalConnection = ratePerAdditionalConnection;
+        this.baseMinutes = baseMinutes;
+        this.ratePerExcessMinute = ratePerExcessMinute;
+    }
+}
 public class BillCalculator {
-    public double calculateBill(final String planType, final int numberOfConnections, final int actualMinutes) {
+    public double calculateBill(final PlanType planType, final int numberOfConnections, final int actualMinutes) {
         int additionalConnections = numberOfConnections - 1;
-        if (planType.equals("Platinum")) {
-            int excessMinutes = getExcessMinutes(actualMinutes, 1500);
-            return 999 + (double) (additionalConnections * 199) + (excessMinutes * 1.30) ;
-        }
-        if (planType.equals("Gold")) {
-            int excessMinutes = getExcessMinutes(actualMinutes, 1000);
-            return 799 + (double) (additionalConnections * 299) + (excessMinutes * 2.45) ;
-        }
-        if (planType.equals("Silver")) {
-            int excessMinutes = getExcessMinutes(actualMinutes, 500);
-            return 599 + (double) (additionalConnections * 399) + (excessMinutes * 3.60);
-        }
-        return 0;
+        int excessMinutes = getExcessMinutes(actualMinutes, planType.baseMinutes);
+        return planType.baseRate + (double) (additionalConnections * planType.ratePerAdditionalConnection) + (excessMinutes * planType.ratePerExcessMinute);
     }
 
     private static int getExcessMinutes(int actualMinutes, int baseMinutes) {
--- src/test/java/com/nelkinda/BillCalculatorTest.java
+++ src/test/java/com/nelkinda/BillCalculatorTest.java
@@ -2,6 +2,7 @@ package com.nelkinda;
 
 import org.junit.jupiter.api.Test;
 
+import static com.nelkinda.PlanType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BillCalculatorTest {
@@ -9,73 +10,73 @@ class BillCalculatorTest {
     @Test
     void testPlatinumSingleConnectionReturns999() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(999, billCalculator.calculateBill("Platinum", 1, 0));
+        assertEquals(999, billCalculator.calculateBill(PLATINUM, 1, 0));
     }
 
     @Test
     void testPlatinumTwoConnectionReturns1198() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(1198, billCalculator.calculateBill("Platinum", 2, 0));
+        assertEquals(1198, billCalculator.calculateBill(PLATINUM, 2, 0));
     }
 
     @Test
     void testGoldSingleConnectionReturns799() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(799, billCalculator.calculateBill("Gold", 1, 0));
+        assertEquals(799, billCalculator.calculateBill(GOLD, 1, 0));
     }
 
     @Test
     void testGoldTwoConnectionReturns1098() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(1098, billCalculator.calculateBill("Gold", 2, 0));
+        assertEquals(1098, billCalculator.calculateBill(GOLD, 2, 0));
     }
 
     @Test
     void testSilverSingleConnectionReturns599() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(599, billCalculator.calculateBill("Silver", 1, 0));
+        assertEquals(599, billCalculator.calculateBill(SILVER, 1, 0));
     }
 
     @Test
     void testSilverTwoConnectionReturns998() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(998, billCalculator.calculateBill("Silver", 2, 0));
+        assertEquals(998, billCalculator.calculateBill(SILVER, 2, 0));
     }
 
     @Test
     void testPlatinumSingleConnectionWith1500MinutesReturns999() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(999, billCalculator.calculateBill("Platinum", 1, 999));
+        assertEquals(999, billCalculator.calculateBill(PLATINUM, 1, 999));
     }
 
     @Test
     void testPlatinumSingleConnectionWith1530MinutesReturns1038() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(1038, billCalculator.calculateBill("Platinum", 1, 1530));
+        assertEquals(1038, billCalculator.calculateBill(PLATINUM, 1, 1530));
     }
 
     @Test
     void testGoldSingleConnectionWith1000MinutesReturns799() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(799, billCalculator.calculateBill("Gold", 1, 1000));
+        assertEquals(799, billCalculator.calculateBill(GOLD, 1, 1000));
     }
 
     @Test
     void testGoldSingleConnectionWith1010MinutesReturns823point5() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(823.5, billCalculator.calculateBill("Gold", 1, 1010));
+        assertEquals(823.5, billCalculator.calculateBill(GOLD, 1, 1010));
     }
 
     @Test
     void testSilverSingleConnectionWith500MinutesReturns599() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(599, billCalculator.calculateBill("Silver", 1, 500));
+        assertEquals(599, billCalculator.calculateBill(SILVER, 1, 500));
     }
 
     @Test
     void testSilverSingleConnectionWith520MinutesReturns671() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(671, billCalculator.calculateBill("Silver", 1, 520));
+        assertEquals(671, billCalculator.calculateBill(SILVER, 1, 520));
     }
 
 }
