7efebf98e50b773b6f9946f99075789c04fcdad3
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2022-09-20 09:38:00 +0530

    Platinum with additional minutes

--- src/main/java/com/nelkinda/BillCalculator.java
+++ src/main/java/com/nelkinda/BillCalculator.java
@@ -1,11 +1,15 @@
 package com.nelkinda;
 
 public class BillCalculator {
-    public double calculateBill(final String planType, final int numberOfConnections) {
+    public double calculateBill(final String planType, final int numberOfConnections, final int minutes) {
         int additionalConnections = numberOfConnections - 1;
-        if (planType.equals("Platinum"))
-            return 999 + (double)(additionalConnections * 199);
-        else
+        if (planType.equals("Platinum")) {
+            int excessMinutes = minutes > 1500 ? minutes - 1500 : 0;
+            return 999 + (double) (additionalConnections * 199) + (excessMinutes * 1.30) ;
+        }
+        if (planType.equals("Gold"))
             return 799 + (double)(additionalConnections * 299);
+        else
+            return 599 + (double)(additionalConnections * 399);
     }
 }
--- src/test/java/com/nelkinda/BillCalculatorTest.java
+++ src/test/java/com/nelkinda/BillCalculatorTest.java
@@ -9,24 +9,43 @@ class BillCalculatorTest {
     @Test
     void testPlatinumSingleConnectionReturns999() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(999, billCalculator.calculateBill("Platinum", 1));
+        assertEquals(999, billCalculator.calculateBill("Platinum", 1, 0));
     }
 
     @Test
     void testPlatinumTwoConnectionReturns1198() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(1198, billCalculator.calculateBill("Platinum", 2));
+        assertEquals(1198, billCalculator.calculateBill("Platinum", 2, 0));
     }
 
     @Test
     void testGoldSingleConnectionReturns799() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(799, billCalculator.calculateBill("Gold", 1));
+        assertEquals(799, billCalculator.calculateBill("Gold", 1, 0));
     }
 
     @Test
     void testGoldTwoConnectionReturns1098() {
         BillCalculator billCalculator = new BillCalculator();
-        assertEquals(1098, billCalculator.calculateBill("Gold", 2));
+        assertEquals(1098, billCalculator.calculateBill("Gold", 2, 0));
     }
+
+    @Test
+    void testSilverSingleConnectionReturns599() {
+        BillCalculator billCalculator = new BillCalculator();
+        assertEquals(599, billCalculator.calculateBill("Silver", 1, 0));
+    }
+
+    @Test
+    void testSilverTwoConnectionReturns998() {
+        BillCalculator billCalculator = new BillCalculator();
+        assertEquals(998, billCalculator.calculateBill("Silver", 2, 0));
+    }
+
+    @Test
+    void testPlatinumSingleConnectionWith1530MinutesReturns1038() {
+        BillCalculator billCalculator = new BillCalculator();
+        assertEquals(1038, billCalculator.calculateBill("Platinum", 1, 1530));
+    }
+
 }
